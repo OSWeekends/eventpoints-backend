19832c786b97973d48ef955d389b1e9810b65a35
Sejas <antonio@sejas.es>
2018-10-18 22:33:38 +0100

    try zip

--- functions/events/events.js
+++
@@ -1,9 +0,0 @@
-const events = require("./events.json")
-
-exports.handler = function(event, context, callback) {
-  callback(null, {
-    statusCode: 200,
-    headers: { "Content-Type": "application/json" },
-    body: events
-  })
-}
