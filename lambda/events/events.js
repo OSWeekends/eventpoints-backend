63b159a430bea4220fe4f1e3f358c5e3a043995d
Sejas <antonio@sejas.es>
2018-10-18 22:49:59 +0100

    trying zip again

---
+++ lambda/events/events.js
@@ -0,0 +1,9 @@
+const events = require("./events.json")
+
+exports.handler = function(event, context, callback) {
+  callback(null, {
+    statusCode: 200,
+    headers: { "Content-Type": "application/json" },
+    body: "yay"
+  })
+}
