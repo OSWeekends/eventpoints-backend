089957e8df1d38293c855c3a780d2382d596da0f
Sejas <antonio@sejas.es>
2018-10-18 23:01:02 +0100

    return single event

--- lambda/events/events.js
+++ lambda/events/events.js
@@ -1,9 +1,11 @@
 const events = require("./events.json")
 
 exports.handler = function(event, context, callback) {
+  const response = "/events" === event.path ? events : events[2]
+
   callback(null, {
     statusCode: 200,
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(events)
+    body: JSON.stringify(response)
   })
 }
