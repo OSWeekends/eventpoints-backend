b1ecf28f9b0a2ecb24e93c096057e1fff4aa02ee
Daniel García Jones <danielgj@gmail.com>
2018-11-01 13:05:11 +0100

    Nueva configuración de GoblinDB y modoDebug para trazas del harmonizer

--- api/api.js
+++ api/api.js
@@ -1,81 +1,101 @@
-var project = require('pillars'),
-    GDB = require('goblindb'),
+const project = require('pillars'),
+    GDB = require('@goblindb/goblindb'),
     exec = require('child_process').exec,
     Scheduled = require('scheduled'),
     harmonizer = require('./harmonizer.js');
     
 // Starting the project
-var eventsApi = project.services.get('http').configure({
+const eventsApi = project.services.get('http').configure({
     port: process.env.PORT || 3000
 })
 
 eventsApi.start();
 
-var goblinDB = GDB();
-var data = goblinDB.get("events");
+const dbConfig = {
+    fileName: 'event_points'
+};
 
-// Define Rutes
-var pingRoute = require('./routes/index');
-var eventsRoute = require('./routes/events')(goblinDB);
-var eventByIdRoute = require('./routes/eventById')(goblinDB);
-var sourcesRoute = require('./routes/sources')();
+const debugMode = false;
 
-// Adding routes objects to the project
-project.routes.add(pingRoute);
-project.routes.add(eventsRoute);
-project.routes.add(eventByIdRoute);
-project.routes.add(sourcesRoute);
 
-//Define here the array of scrappers
-var spiders = ['meetup'];
+const goblinDB = GDB(dbConfig, err => {
 
-// Cron Tasks
- var pythonRocks = new Scheduled({
-    id: "pythonRocks",
-    pattern: "0 0/5 0 ? * * *",
-    task: function() {
-        console.log(`---- Borro ficheros json! ------`);
-        exec('cd ../scrapers/output && rm *.json', function(error, stdout, stderr) {
-            if (error) {
-                console.log('Borrando error: ' + error);
-            } else {
-                spiders.forEach(function (spider) {                    
-                    console.log(`---- Proceso hijo de ${spider} Iniciado! ------`);
-                    exec('cd ../scrapers/ && scrapy crawl ' + spider + ' -o ../scrapers/output/' + spider + '.json', function(error, stdout, stderr) {
-                        console.log(`---- Proceso hijo de ${spider} terminado! -----`);
-                        if (stdout) {
-                            console.log('stdout: ' + stdout);
-                        }
-        
-                        if (stderr) {
-                            console.log('stderr: ' + stderr);
-                        }
-        
-                        if (error) {
-                            console.log('exec error: ' + error);
-                        }
-                    });     
-                });
-            }
-
-        });        
+    if(err) {
+        console.log("Error launching DB - will exit");
+        exit();
     }
-}).start();
 
+    var data = goblinDB.get("events");
 
-var harmonizerTask = new Scheduled({
-    id: "harmonizerTask",
-    pattern: "0 1/5 0 ? * * *",
-    task: function() {
-        harmonizer(goblinDB);
+    if(!data) {
+        data = [];
+        goblinDB.set(data, "events");
     }
-}).start();
 
-goblinDB.on('change', function(){
-    data = goblinDB.get("events");
+    // Define Rutes
+    const pingRoute = require('./routes/index');
+    const eventsRoute = require('./routes/events')(data);
+    const eventByIdRoute = require('./routes/eventById')(data);
+    const sourcesRoute = require('./routes/sources')();
+
+    // Adding routes objects to the project
+    project.routes.add(pingRoute);
+    project.routes.add(eventsRoute);
+    project.routes.add(eventByIdRoute);
+    project.routes.add(sourcesRoute);
+
+    //Define here the array of scrappers
+    const spiders = ['meetup'];
+
+    // Cron Tasks
+    const pythonRocks = new Scheduled({
+        id: "pythonRocks",
+        pattern: "0 0/5 0 ? * * *",
+        task: function() {
+            console.log(`---- Borro ficheros json! ------`);
+            exec('cd ../scrapers/output && rm *.json', function(error, stdout, stderr) {
+                if (error) {
+                    console.log('Borrando error: ' + error);
+                } else {
+                    spiders.forEach(function (spider) {                    
+                        console.log(`---- Proceso hijo de ${spider} Iniciado! ------`);
+                        exec('cd ../scrapers/ && scrapy crawl ' + spider + ' -o ../scrapers/output/' + spider + '.json', function(error, stdout, stderr) {
+                            console.log(`---- Proceso hijo de ${spider} terminado! -----`);
+                            if (stdout) {
+                                console.log('stdout: ' + stdout);
+                            }
+            
+                            if (stderr) {
+                                console.log('stderr: ' + stderr);
+                            }
+            
+                            if (error) {
+                                console.log('exec error: ' + error);
+                            }
+                        });     
+                    });
+                }
+
+            });        
+        }
+    }).start();
+
+    const harmonizerTask = new Scheduled({
+        id: "harmonizerTask",
+        pattern: "0 * 0 ? * * *",
+        task: function() {
+            harmonizer(goblinDB, debugMode);
+        }
+    }).start();
+
+    // Events supported
+    goblinDB.on('change', function(changes){
+        data = goblinDB.get("events");
+    });
+
+    harmonizerTask.launch();
+    //pythonRocks.launch();
+
 });
-
-harmonizerTask.launch();
-pythonRocks.launch();
 
 module.exports = eventsApi;
--- api/harmonizer.js
+++ api/harmonizer.js
@@ -1,9 +1,21 @@
-var _ = require('lodash'),
+const _ = require('lodash'),
     uuidV4 = require('uuid/v4'),
     fs = require('fs');
 
-module.exports = function(goblinDB) {
-    var arrayEventos = [];
+module.exports = function(goblinDB, debugMode) {
+    
+    if(debugMode) {
+        console.log('Ejecuto el harmonizer');
+    }
+
+    const currentEventos = goblinDB.get("events");
+
+    if(debugMode) {
+        console.log("************ Actualmente en BD: " + currentEventos.length);
+    }
+
+    var newEventos = [];
+
     fs.readdir('../scrapers/output', function (err, files) {
         if(err){
             console.log("ERROR reading ../scrapers/output:", err);
@@ -11,11 +23,7 @@ module.exports = function(goblinDB) {
 
             files.forEach(function(file){
 
-                
-            
                 if (/.json/.test(file)) {
-
-                    //Load refreshed file
                     var currentArray = [];
 
                     try {
@@ -26,24 +34,38 @@ module.exports = function(goblinDB) {
                     }
 
                     if (currentArray.length > 0) {
-                        arrayEventos = _.concat(arrayEventos, currentArray);
+                        newEventos = _.concat(newEventos, currentArray);
                     }
                     
                 }
             });
     
+            // adding UUIDs
+            newEventos.forEach(function(event) {
+                event.id = uuidV4();
+            });
+
+            if(debugMode) {
+                console.log("************ Nuevos eventos: " + newEventos.length);
+            }
+
+            var arrayEventos = _.concat(newEventos, currentEventos);
+
+            if(debugMode) {
+                console.log("************ Tras juntar eventos: " + arrayEventos.length);
+            }
+
             /*
                 Filtering duplicate elements
                 @see https://lodash.com/docs/4.17.4#uniqBy
             */
-            //arrayEventos = _.uniqBy(arrayEventos, "target_url");
+           arrayEventos = _.uniqBy(arrayEventos, "target_url");
+
+           if(debugMode) {
+                console.log("************ Tras eliminar eventos duplicados: " + arrayEventos.length);
+           }
     
-            // adding UUIDs
-            arrayEventos.forEach(function(event) {
-                event.id = uuidV4();
-            });
-    
-            goblinDB.set(arrayEventos, "events");
+           goblinDB.set(arrayEventos, "events");
         }
     });
 
