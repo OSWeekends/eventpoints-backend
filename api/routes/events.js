9eec6ff7d3f4e97de0e7df856aadbfaa8ad1af3d
Daniel García Jones <danielgj@gmail.com>
2018-11-01 21:49:41 +0100

    Filtrado de eventos por fecha de inicio y fin sin validación

--- api/routes/events.js
+++ api/routes/events.js
@@ -5,7 +5,26 @@ module.exports = function(data) {
         path: 'api/v1/events',
         cors: true
     }, function(gw) {
-        gw.json(data, {
+        const from = gw.req.query.from;
+        const to = gw.req.query.to;
+
+        var filteredData = data;
+
+        if(from) {
+            console.log("From: " + from);
+            filteredData = filteredData.filter(event => {
+                return new Date(event.date) >= new Date(from);                
+            });
+        }
+        
+        if(to) {
+            console.log("To: " + to);
+            filteredData = filteredData.filter(event => {
+                return new Date(event.date) <= new Date(to);                
+            });
+        }
+        
+        gw.json(filteredData, {
             deep: 10
         });
     });
