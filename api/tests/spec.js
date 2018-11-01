22963467ce01880dbe816260a3fe9d21154c5451
Daniel García Jones <danielgj@gmail.com>
2018-11-01 13:34:44 +0100

    Corrected API tests

--- api/tests/spec.js
+++ api/tests/spec.js
@@ -1,10 +1,9 @@
 var request = require('superagent');
 var assert = require('assert');
 
-describe('loading express', function () {
+describe('TEST EVENT API', function () {
   var server = require('../api');
   var baseUrl = 'http://localhost:3000/api/v1';
-  var existingId = 'b63ba480-f937-4ac2-8f16-46ce738a5231';
 
   before(function () {
     server = require('../api');
@@ -15,14 +14,23 @@ describe('loading express', function () {
     process.exit();
   });
 
+
+  it('Giving time to start the server', function testSlash(done) {
+    request
+      .get(baseUrl)
+      .end((err, res) => {
+        assert.equal(res.status, 404);
+        done();
+      });
+  });
+
   it('responds to /api/v1', function testSlash(done) {
-  request
-    .get(baseUrl)
-    .end((err, res) => {
-      assert.equal(res.status, 200);
-      done();
-    });
-    
+    request
+      .get(baseUrl)
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        done();
+      });
   });
 
   it('responds to GET Events', function testSlash(done) {
