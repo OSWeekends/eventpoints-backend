60ee7bd73bb0dc7cda6c6375cb4ad3eaa4b09bd8
Carlos Fernandez <carlosfdev@gmail.com>
2018-11-08 22:21:39 +0100

    limpiados los .pyc, añadidas coordenadas al spider y actualizado el test_data.json

--- scrapers/eventscraper/spiders/meetup.py
+++ scrapers/eventscraper/spiders/meetup.py
@@ -1,5 +1,6 @@
 import scrapy
 import textwrap
+from urllib.parse import urlparse, parse_qs
 from scrapy.spiders import CrawlSpider
 from eventscraper.items import Event
 
@@ -14,7 +15,6 @@ class MeetupSpider(CrawlSpider):
         for tech_meetup in tech_meetups:
             event = Event()
             event['source'] = {}
-            event['source']['event_url'] = response.request.url
             #TODO: Seguramente toda esta info debería de añadirla la API, aquí podríamos simplemente indicar el ID de source.
             event['source']['name'] = 'Meetup'
             event['source']['logo'] = 'https://secure.meetupstatic.com/s/img/5455565085016210254/logo/svg/logo--script.svg'
@@ -31,12 +31,16 @@ class MeetupSpider(CrawlSpider):
     def parse_details(response):
         event = response.meta['event']
 
+        event['source']['event_url'] = response.request.url
+
         details = response.xpath('//div[contains(@class, "event-description")]/p').extract_first()
         event['host'] = response.xpath('//div[contains(@class, "event-info-hosts-text")]/a/span/span/span/text()').extract_first()
         event['short_details'] = textwrap.shorten(details, width=150, placeholder="...")
         event['details'] = details
 
         event['price'] = {}
+        event['price']['details'] = '0'
+        event['price']['is_trusted'] = True
         event['price']['is_free'] = True
 
         #TODO: Podemos añadir la hora inicio y la hora fin
@@ -47,8 +51,15 @@ class MeetupSpider(CrawlSpider):
         event['location'] = {}
         event['location']['name'] = location[0]
         event['location']['address'] = location[1]
-        #TODO: Desharcodear la latitud y longitud.
-        event['location']['lat'] = '40.392303'
-        event['location']['lng'] = '-3.697430'
+
+        google_maps_url = response.xpath('//a[contains(@class, "venueDisplay")]/@href').extract_first()
+        google_maps_url_params = parse_qs(urlparse(google_maps_url.replace('%2C', ',')).query)
+        event['location']['query'] = google_maps_url_params['query'][0]
+
+        #TODO: A veces no vienen las coordenadas en la URL, buscar alternativa
+        google_maps_coordinates = google_maps_url_params['query'][0].split(',')
+
+        event['location']['lat'] = google_maps_coordinates[0]
+        event['location']['lng'] = google_maps_coordinates[1]
 
         yield event
