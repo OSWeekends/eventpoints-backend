70f5bde3ce249509b13ce37551556b2d0557c808
Daniel García Jones <danielgj@gmail.com>
2018-11-18 12:31:20 +0100

    Cambiada ejecución de los cron a 5 minutos

--- api/api.js
+++ api/api.js
@@ -58,7 +58,7 @@ const goblinDB = GDB(config.dbConfig, err => {
     // 0 30 11 * * (a las 11:30 todos los día)
     const pythonRocks = new Scheduled({
         id: "pythonRocks",
-        pattern: "*/45 * * * *",
+        pattern: "*/4 * * * *",
         task: function() {
             console.log(`---- Borro ficheros json! ------`);
             exec('cd ../scrapers/output && rm *.json', function(error, stdout, stderr) {
@@ -95,7 +95,7 @@ const goblinDB = GDB(config.dbConfig, err => {
     // 0 0 12 * * (a las 12:00 todos los día)
     const harmonizerTask = new Scheduled({
         id: "harmonizerTask",
-        pattern: "*/55 * * * *",
+        pattern: "*/8 * * * *",
         task: function() {
             harmonizer(goblinDB, sources, config.debugMode);
         }
