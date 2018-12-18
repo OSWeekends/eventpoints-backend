e7ca2747b37bb5932b27580d1a4b82eabbe9a1fa
Daniel García Jones <danielgj@gmail.com>
2018-12-18 21:54:24 +0100

    Añadida fuente de medialab prado

--- api/sources.js
+++ api/sources.js
@@ -4,5 +4,11 @@ module.exports = [
         "name": "Meetup",
         "logo": "https://secure.meetupstatic.com/s/img/5455565085016210254/logo/svg/logo--script.svg",
         "url": "https://www.meetup.com/es-ES/"
+    },
+    {
+        "id": "medialab-prado",
+        "name": "medialab-prado",
+        "logo": "https://www.medialab-prado.es/themes/custom/medialab_theme/img/logotipo@2x.jpg",
+        "url": "https://www.medialab-prado.es/"
     }
 ];
