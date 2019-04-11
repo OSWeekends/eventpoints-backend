da12985402a3d50f61ec841917107c25745fc4ea
Daniel Garcia Jones <danielgj@gmail.com>
2019-04-11 07:40:42 +0200

    Deshabilitado temporal de la fuente de eventbrite

--- api/sources.js
+++ api/sources.js
@@ -4,11 +4,5 @@ module.exports = [
         "name": "Meetup",
         "logo": "https://secure.meetupstatic.com/s/img/5455565085016210254/logo/svg/logo--script.svg",
         "url": "https://www.meetup.com/es-ES/"
-    },
-    {
-        "id": "eventbrite",
-        "name": "Eventbrite",
-        "logo": "https://www.eventbrite.com/wp-content/themes/core/img/blog-logo2x.png",
-        "url": "https://www.eventbrite.es/"
     }
 ];
