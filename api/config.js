785b472a982fb0377a9bf1bdee016c2b4ed9c352
Daniel Garcia <daniel@bdeo.es>
2019-04-28 11:10:18 +0200

    Cambiado formato parámetros config

--- api/config.js
+++ api/config.js
@@ -4,7 +4,7 @@ module.exports = {
         fileName: 'event_points'
     },
     harmonizerCron: "*/55 * * * *", //(cada 55 minutos)
-    scraperCron: "*/45 * * * *", //(cada 45 minutos)
+    pythonCron: "*/45 * * * *", //(cada 45 minutos)
     port: 3000,
     mockupData: false
 }
