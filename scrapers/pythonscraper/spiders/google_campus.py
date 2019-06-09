dfb31791555fc9355718ac8a8046586be27030eb
Daniel Garcia Jones <danielgj@gmail.com>
2019-06-09 11:47:53 +0200

    Añadidos campos en la respuesta de campus

--- scrapers/pythonscraper/spiders/google_campus.py
+++ scrapers/pythonscraper/spiders/google_campus.py
@@ -16,6 +16,7 @@ class GoogleCampusSpyder(CrawlSpider):
 
     def parse(self, response):
         resp_formateada = response.body_as_unicode()[6:] # elimina )]}', al principio del json
+
         jsonresponse = json.loads(resp_formateada)
 
         for obj in jsonresponse['objects']:
@@ -24,16 +25,25 @@ class GoogleCampusSpyder(CrawlSpider):
             event['group'] = obj['host_company_name']
             event['source'] = 'google_campus'
             event['abstract'] = obj['description_preview']
+            event['target_url'] = 'https://www.campus.co/madrid/es/events/' + obj['_key']
+            event['host'] = obj['host_company_name']
+            event['abstract_details'] = ''
             event['datetime'] =  self.getTimeStamp(obj['local_start_str'])#obj['local_start_str'].split('T')[0]
             event['location'] = {}
+            event['location']['name'] = 'Google Campus'
             event['location']['address'] = 'Calle Moreno Nieto, 2, 28005 Madrid'
             event['location']['lat'] = '40.4124265'
             event['location']['lng'] = '-3.7204109'
+            event['location']['query'] = '40.4124265,-3.7204109'
+            event['price'] = {}
+            event['price']['details'] = '0'
+            event['price']['is_trusted'] = True
+            event['price']['is_free'] = True
             yield event
 
     def getTimeStamp(self, date_str): 
         if (date_str is None):
             return None
-        date  = parse(date_str)#datetime.strptime(date_str, '%Y-%m-%dT%H:%M:%S')
+        date  = parse(date_str)
         date_str = str(calendar.timegm(date.utctimetuple()))
         return date_str+'000'
