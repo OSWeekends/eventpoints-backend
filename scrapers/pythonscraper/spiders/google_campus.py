f82de21f6b27083d28cec4a0ff1426ba6bea0f29
Edmund Home <edmundhome@hotmail.com>
2019-06-17 20:16:59 +0200

    descripción añadida a google_campus

--- scrapers/pythonscraper/spiders/google_campus.py
+++ scrapers/pythonscraper/spiders/google_campus.py
@@ -2,7 +2,7 @@ import scrapy
 import textwrap
 import json
 import calendar
-from pprint import pprint
+import re
 from urllib.parse import urlparse, parse_qs
 from scrapy.spiders import CrawlSpider
 from pythonscraper.items import Event
@@ -15,15 +15,13 @@ class GoogleCampusSpyder(CrawlSpider):
     start_urls = ['https://www.campus.co/api/campuses/madrid/events/v2']
 
     def parse(self, response):
-        resp_formateada = response.body_as_unicode()[6:] # elimina )]}', al principio del json
-
-        jsonresponse = json.loads(resp_formateada)
+        jsonresponse = self.cleanjson(response)
 
         for obj in jsonresponse['objects']:
             event = Event()
+            event['source'] = 'google_campus'
             event['title'] = obj['name']
             event['group'] = obj['host_company_name']
-            event['source'] = 'google_campus'
             event['abstract'] = obj['description_preview']
             event['target_url'] = 'https://www.campus.co/madrid/es/events/' + obj['_key']
             event['host'] = obj['host_company_name']
@@ -39,7 +37,8 @@ class GoogleCampusSpyder(CrawlSpider):
             event['price']['details'] = '0'
             event['price']['is_trusted'] = True
             event['price']['is_free'] = True
-            yield event
+            event_page = self.start_urls[0] + '/' + obj['_key']
+            yield scrapy.Request(event_page, callback=self.parse_details, meta={'event' : event})
 
     def getTimeStamp(self, date_str): 
         if (date_str is None):
@@ -47,3 +46,19 @@ class GoogleCampusSpyder(CrawlSpider):
         date  = parse(date_str)
         date_str = str(calendar.timegm(date.utctimetuple()))
         return date_str+'000'
+
+    def parse_details(self, response):
+        jsonresponse = self.cleanjson(response)
+
+        event = response.meta['event']
+        event['abstract_details'] = self.cleanhtml(jsonresponse['objects'][0]['description'])
+        yield event
+    
+    def cleanjson(self, raw_json):
+        resp_formateada = raw_json.body_as_unicode()[6:] # elimina )]}', al principio del json
+        return json.loads(resp_formateada)
+
+    def cleanhtml(self, raw_html):
+        cleanr = re.compile('<.*?>')
+        cleantext = re.sub(cleanr, '', raw_html)
+        return cleantext
