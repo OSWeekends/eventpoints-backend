533db58b8b02243bd1e042a207a773949a1c9a0b
Daniel Garcia Jones <danielgj@gmail.com>
2019-07-14 10:14:36 +0200

    Desactivadas fuentes de Google y Medialab

--- api/sources.js
+++ api/sources.js
@@ -12,19 +12,5 @@ module.exports = [
         "name": "Eventbrite",
         "logo": "https://www.eventbrite.com/wp-content/themes/core/img/blog-logo2x.png",
         "url": "https://www.eventbrite.es/"
-    },
-    {
-        "id": "medialab_prado",
-        "type": "r",
-        "name": "Medialab Prado",
-        "logo": "https://www.medialab-prado.es/themes/custom/medialab_theme/img/logotipo@2x.jpg",
-        "url": "https://www.medialab-prado.es/"
-    },
-    {
-        "id": "google_campus",
-        "type": "py",
-        "name": "Google Campus",
-        "logo": "",
-        "url": "https://www.campus.co/madrid/es/"
     }
 ];
