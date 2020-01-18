84cb5325914214d2ebb3882b034a2f3b29d6cc78
Daniel <danielgj@gmail.com>
2020-01-18 14:28:51 +0100

    Fix #67 Ruta por fuente

--- api/routes/eventBySource.js
+++ api/routes/eventBySource.js
@@ -1,4 +1,4 @@
-const _ = require('lodash');
+const moment = require('moment');
 
 module.exports = function(config, goblinDB) {
   const eventsRouter = new Route(
@@ -8,17 +8,56 @@ module.exports = function(config, goblinDB) {
       cors: true,
     },
     function(gw) {
-      const data = goblinDB.get('events');
+      const fromValue = gw.req.query.from;
+      const toValue = gw.req.query.to;
+
+      const rawData = goblinDB.get('events');
+      let filteredData = rawData.filter(
+        item => item.source && item.source.id === gw.params.path
+      );
+      if (fromValue) {
+        const from = moment(fromValue).isValid()
+          ? moment(fromValue)
+          : moment(fromValue, 'x');
+        if (from.isValid()) {
+          filteredData = filteredData.filter(event =>
+            moment(event.datetime).isAfter(from)
+          );
+        } else {
+          gw.statusCode = 400;
+          gw.json({ msg: 'La fecha inicio es incorrecta' });
+        }
+      }
+
+      if (toValue) {
+        const to = moment(toValue).isValid()
+          ? moment(toValue)
+          : moment(toValue, 'x');
+        if (to.isValid()) {
+          filteredData = filteredData.filter(event =>
+            moment(event.datetime).isBefore(to)
+          );
+        } else {
+          gw.statusCode = 400;
+          gw.json({ msg: 'La fecha fin es incorrecta' });
+        }
+      }
+
+      // Si no pasamos fechas de inicio, se devuelve todos los eventos a futuro
+      if (!fromValue && !toValue) {
+        filteredData = filteredData.filter(event =>
+          moment(event.datetime).isSameOrAfter()
+        );
+      }
 
       if (config.mockupData) {
-        gw.json(data, {
+        gw.json([], {
           deep: 10,
         });
       } else {
-        const items = data.filter(
-          item => item.source && item.source.id === gw.params.path
-        );
-        gw.json(items);
+        gw.json(filteredData, {
+          deep: 10,
+        });
       }
     }
   );
